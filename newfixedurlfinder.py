229aa6f0489d6643049a4b2b634e52abcabd2792
memerememe <47179281+memerememe@users.noreply.github.com>
2020-12-01 13:48:04 +0800

    Update newfixedurlfinder.py

--- newfixedurlfinder.py
+++ newfixedurlfinder.py
@@ -1,17 +1,49 @@
 import requests
-from html.parser import HTMLParser
 from bs4 import BeautifulSoup
-r = requests.get("https://weather.com/zh-TW/weather/today/l/TWXX0021:1:TW?Goto=Redirected")
-#the below line has been commented cuz its optional and mostly not needed.
-#r2 = requests.get('https://weather.com/hr-HR/weather/today/l/TWXX0021:1:TW?Goto=Redirected')
-#replace de-DE in the below link with appropriate locale(https://saimana.com/list-of-country-locale-code/) and get url
-r3 = requests.get("https://weather.com/en-IN/").text
-source = r3
+import urllib.request
+from html.parser import HTMLParser
+import string
+#todo: implement to check if country is valid or not, check if option entered is valid or not
+x = ""
+fulltext=""
+country = input("enter your country:")
+url = 'https://weather.codes/'+country+'/'
+reqs = requests.get(url)
+soupu = BeautifulSoup(reqs.text, 'lxml')
+inman = input("Enter your location: ")
+#changes every first letter in input to capital
+capinman = string.capwords(inman)
+#find li tag and parses location from it
+for tag in soupu.find_all("li"):
+    x = tag.text+"\n"
+    if capinman in x:
+       fulltext = fulltext+x + "\n"
+#stores to list so can be accessed with option of user
+listya = list(filter(bool, fulltext.splitlines()))
+#gets length of list for "if condition" so we can check if option valid or not which i didnt know how to implement
+lengthoflist = len(listya)
+#prints the locations
+print(fulltext)
+#asks for location option
+optionoflocation = input("Enter your option: ")
+#minuses one from option for list selecting
+listoption = int(optionoflocation)-1
+optiontest = str(listya[listoption])
+code = optiontest[-8:]
+storageout=""
+
+#gets page with weathercode obtained
+source = requests.get('https://weather.com/weather/today/l/'+code).text
+
 soup = BeautifulSoup(source, 'lxml')
+#parses using bs4
 links = soup.find('a', class_='ListItem--listItem--1r7mf styles--listItem--2JY_Z Button--default--2yeqQ')
 links5 = str(links)
 soup = BeautifulSoup(links5, "html.parser")
 for links5 in soup.findAll('a'):
     x = 'https://weather.com'+links5.get('href')
-    print(x)
+    #store so that can be accessed outside loop
+    storageout=storageout+x
+#finally prints url
+print(storageout)
 
