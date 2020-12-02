0a5ba4e15bfd780580b412a2b99792895f5e35a2
TheProgrammingArchive <55741209+TheProgrammingArchive@users.noreply.github.com>
2020-12-02 10:18:09 +0530

    Delete grab.py

--- wip/grab.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-r = requests.get("https://weather.com/zh-TW/weather/today/l/TWXX0021:1:TW?Goto=Redirected")
-# the below line has been commented cuz its optional and mostly not needed.
-# r2 = requests.get('https://weather.com/hr-HR/weather/today/l/TWXX0021:1:TW?Goto=Redirected')
-# replace de-DE in the below link with appropriate locale(https://saimana.com/list-of-country-locale-code/) and get url
-r3 = requests.get("https://weather.com/de-DE/")
-print(r3.url)
